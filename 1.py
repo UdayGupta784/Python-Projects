df3516bc7a221f34dfd5a6a03ac823c7844c4eb6
UdayGupta784 <guptauday564@gmail.com>
2026-01-19 21:18:17 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,45 @@
+#ROCK PAPER SCISSIORS GAME
+import random
+options = ["rock","paper","scissors"]
+player_win = 0
+computer_win = 0
+tie = 0
+while True:
+    player_choice = input("Enter(Rock,Paper Or Scissors) or (Q to Quit): ").strip().lower()
+    
+    computer_choice = random.choice(options)
+    if player_choice== "q":
+        print("GAME OVER")
+        print(f"SUMMARY:  YOU WON- {player_win}, COMPUTER WON- {computer_win} AND GAME TIED- {tie}")
+        break
+    
+    print(f"Computer chose: {computer_choice}")
+    if player_choice == computer_choice:
+        print("Game Tied")
+        tie+=1
+
+    elif player_choice=="rock" and computer_choice== "scissors":
+        print("You Won")
+        player_win+=1
+
+    elif player_choice=="rock" and computer_choice== "paper":
+        print("You Lose")
+        computer_win+=1
+
+    elif player_choice=="scissors" and computer_choice== "rock":
+        print("You Lose")
+
+        computer_win+=1
+        
+    elif player_choice=="scissors" and computer_choice== "paper":
+        print("You Won")
+        player_win+=1
+
+    elif player_choice=="paper" and computer_choice== "scissors":
+        print("You Lose")
+        computer_win+=1
+    else:
+        print("You Won")
+        player_win+=1
+
+    
