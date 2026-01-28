872233c4cd93c1a6cde379de567d0a927d55f70c
UdayGupta784 <guptauday564@gmail.com>
2026-01-28 19:23:04 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,39 +7,30 @@ tie = 0
 while True:
     player_choice = input("Enter(Rock,Paper Or Scissors) or (Q to Quit): ").strip().lower()
     
-    computer_choice = random.choice(options)
+  
     if player_choice== "q":
         print("GAME OVER")
         print(f"SUMMARY:  YOU WON- {player_win}, COMPUTER WON- {computer_win} AND GAME TIED- {tie}")
         break
-    
+    if player_choice not in options:
+        print(" Invalid choice! Try again.\n")
+        continue
+    computer_choice = random.choice(options)
     print(f"Computer chose: {computer_choice}")
     if player_choice == computer_choice:
         print("Game Tied")
         tie+=1
 
-    elif player_choice=="rock" and computer_choice== "scissors":
-        print("You Won")
-        player_win+=1
+     elif(
+        (player_choice == "rock" and computer_choice == "scissors") or
+        (player_choice == "scissors" and computer_choice == "paper") or
+        (player_choice == "paper" and computer_choice == "rock")
+    ):
+         print("You Won!\n")
+         player_win += 1
 
-    elif player_choice=="rock" and computer_choice== "paper":
-        print("You Lose")
-        computer_win+=1
-
-    elif player_choice=="scissors" and computer_choice== "rock":
-        print("You Lose")
-
-        computer_win+=1
-        
-    elif player_choice=="scissors" and computer_choice== "paper":
-        print("You Won")
-        player_win+=1
-
-    elif player_choice=="paper" and computer_choice== "scissors":
-        print("You Lose")
-        computer_win+=1
     else:
-        print("You Won")
-        player_win+=1
+        print("You LOSE/n")
+        computer_win+=1
 
     
